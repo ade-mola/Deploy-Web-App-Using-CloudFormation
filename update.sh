81e65461a279fa82510af0894c514ef599c01830
ade-mola <salamiolokun@gmail.com>
2020-03-28 00:18:08 +0100

    CloudFormation

--- update.sh
+++ update.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-echo "Checking if stack exists ..."
 
-if ! aws cloudformation describe-stacks --region us-west-2 --stack-name $1 ; then
+aws cloudformation update-stack \
+--stack-name $1 \
+--template-body file://$2 \
+--parameters file://$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-west-2
 
-    echo -e "\nStack exists, attempting update ..."
-    aws cloudformation update-stack \
-    --stack-name $1 \
-    --template-body file://$2 \
-    --parameters file://$3 \
-    --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
-    --region=us-west-2
 
-    echo "Waiting for stack update to complete ..."
-    aws cloudformation wait stack-update-complete --stack-name $1 --region=us-west-2
+
